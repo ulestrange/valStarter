34f6940a7dc85d7167603955844e47b286985a89
ulestrange <lestrange.una@itsligo.ie>
2018-11-12 12:51:29 +0000

    Ready to give to the students to start with

---
+++ ValStarter/Pages/CreateStudent.cshtml.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ValStarter.Models;
+
+namespace ValStarter.Pages
+{
+    public class CreateStudentModel : PageModel
+    {
+
+        [BindProperty]
+
+        public Student Student { get; set; }
+        public void OnGet()
+        {
+        }
+    }
+}

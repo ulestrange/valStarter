2efc1adc5e2dabdf475100b7edac5750370a37c2
ulestrange <lestrange.una@itsligo.ie>
2018-11-22 10:28:06 +0000

    Taking values to a second page works using Session set string. However it doesn't work if there are no values set. No error checking is done on either side

---
+++ ValStarter/Pages/ConfirmStudent.cshtml.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ValStarter.Models;
+
+namespace ValStarter.Pages
+{
+    public class ConfirmStudentModel : PageModel
+    {
+        
+        public string StudentID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public void OnGet()
+        {
+            StudentID = HttpContext.Session.GetString("StudentID");
+            FirstName = HttpContext.Session.GetString("FirstName");
+            LastName = HttpContext.Session.GetString("LastName");
+
+        }
+    }
+}
--- ValStarter/Pages/CreateStudent.cshtml.cs
+++ ValStarter/Pages/CreateStudent.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ValStarter.Models;
@@ -16,6 +17,24 @@ namespace ValStarter.Pages
         public Student Student { get; set; }
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                HttpContext.Session.SetString("FirstName", Student.FirstName);
+                HttpContext.Session.SetString("LastName", Student.LastName);
+                HttpContext.Session.SetString("StudentID", Student.StudentID);
+
+
+                return RedirectToPage("ConfirmStudent");
+
+            }
+            else
+            {
+                return Page();
+            }
         }
     }
 }

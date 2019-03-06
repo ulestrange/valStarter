bd8a76cef0b6ba3e58438927397b2fc3c4b491ce
ulestrange <lestrange.una@itsligo.ie>
2019-03-06 14:11:53 +0000

    lauput page chnaged

--- ValStarter/Pages/Index.cshtml.cs
+++ ValStarter/Pages/Index.cshtml.cs
@@ -9,9 +9,17 @@ namespace ValStarter.Pages
 {
     public class IndexModel : PageModel
     {
+
+        public string Message = "Welcome";
         public void OnGet()
         {
+            Response.Cookies.Append("UnaCookie", "Hello World");
 
+            if (Request.Cookies["UnaCookie"] != null)
+            {
+
+                Message = "Welcome back";
+            }
         }
     }
 }
